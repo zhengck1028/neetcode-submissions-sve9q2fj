fcad893b038ad99e4b3db66388b7f99eb92ee88d
zhengck1028 <39734650+zhengck1028@users.noreply.github.com>
2026-04-24 15:39:30 -0400

    Add: regular-expression-matching - submission-1

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(p), len(s)
+        dp = [[False] * (n+1) for _ in range(m+1)] # dp[i][j]: p[:i] matches s[:j]
+        dp[0][0] = True
+        for i in range(1, m + 1):
+            if p[i - 1] == "*":
+                dp[i][0] = dp[i - 2][0]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if p[i-1]==s[j-1] or p[i-1] == ".":
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[i-1] == "*":
+                    dp[i][j] = dp[i-2][j]
+                    if p[i-2] == s[j-1] or p[i-2] == ".":
+                        dp[i][j] = dp[i][j-1] or dp[i][j]
+        return dp[m][n]
