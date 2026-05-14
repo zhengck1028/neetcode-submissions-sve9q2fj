a34f0a891355af8efa39c6d4c09f6b881cb6fcea
zhengck1028 <39734650+zhengck1028@users.noreply.github.com>
2026-05-13 23:38:42 -0400

    Add: burst-balloons - submission-0

---
+++ Data Structures & Algorithms/burst-balloons/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        n = len(nums)
+        new_nums = [1] + nums + [1]
+        dp = [[0] * (n + 2) for _ in range(n + 2)]
+        for l in range(n, 0, -1):
+            for r in range(l, n + 1):
+                for i in range(l, r + 1):
+                    coins = new_nums[l - 1] * new_nums[i] * new_nums[r + 1]
+                    coins += dp[l][i - 1] + dp[i + 1][r]
+                    dp[l][r] = max(dp[l][r], coins)
+        return dp[1][n]
