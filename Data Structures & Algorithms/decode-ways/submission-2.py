f5b3c01de87e3830b8d822121af2613369853f3d
zhengck1028 <39734650+zhengck1028@users.noreply.github.com>
2026-05-29 22:31:29 -0400

    Update: decode-ways - submission-2

--- Data Structures & Algorithms/decode-ways/submission-2.py
+++ Data Structures & Algorithms/decode-ways/submission-2.py
@@ -2,14 +2,13 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         if not s or s[0] == '0':
             return 0
-        prv1, prv2 = 1, 1
+        prv2, prv1 = 1, 1
         for i in range(1, len(s)):
             cur = 0
-            if s[i] != '0':
-                cur += prv2
-            two_digit = int(s[i-1:i+1])
-            if 10 <= two_digit <= 26:
+            if s[i] != "0":
                 cur += prv1
-            prv1 = prv2
-            prv2 = cur
-        return prv2
+            if 10 <= int(s[i-1:i+1]) <= 26:
+                cur = cur + prv2
+            prv2 = prv1
+            prv1 = cur
+        return prv1
